c0e54c527691b77fed944481ab0b56864473a814
Deathlycraft <deathlycraft@deathlyenterprises.com>
2015-09-13 00:30:48 -0500

    New: Initial Commit of Server project.

---
+++ De.Server/De.Server/Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace De.Server
+{
+    public interface IServer
+    {
+        void StartServer();
+        void StopServer();
+    }
+
+    public class Server<TService, TContract> : IServer
+    {
+        private readonly ServiceHost _serviceHost;
+
+        public Server(string hostname, string portNum, string endpoint)
+        {
+            _serviceHost = new ServiceHost(typeof (TService));
+            _serviceHost.AddServiceEndpoint(typeof (TContract), new NetTcpBinding(SecurityMode.None),
+                new Uri($"net.tcp://{hostname}:{portNum}/{endpoint}"));
+        }
+
+        public void StartServer()
+        {
+            try
+            {
+                _serviceHost.Open();
+            }
+            catch (CommunicationException)
+            {
+                _serviceHost.Abort();
+            }
+        }
+
+        public void StopServer()
+        {
+            _serviceHost.Close();
+        }
+    }
+}
